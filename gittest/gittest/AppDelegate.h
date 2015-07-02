d139fce5bb62e12d1402cfddd71fff8a64609c0e
weiyin <weiyin.liang@gmail.com>
2015-07-02 14:51:16 +0800

    添加项目

---
+++ gittest/gittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittest
+//
+//  Created by tukeq on 15/7/2.
+//  Copyright © 2015年 ShiJieBang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittest/gittest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittest
+//
+//  Created by tukeq on 15/7/2.
+//  Copyright © 2015年 ShiJieBang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
